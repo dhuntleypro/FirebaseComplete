34b7c16860ee9dc2bc336ee00c91e4e3a7a71ae4
Darrien Huntley <info@dhuntleyinc.com>
2021-01-14 09:54:20 -0500

    password reset connected/working

--- FirebaseComplete/Firebase/View/ForgotPasswordView.swift
+++ FirebaseComplete/Firebase/View/ForgotPasswordView.swift
@@ -10,22 +10,18 @@ import SwiftUI
 // make sure to always change to project name
 /*
  1. In firebase -
-        1. firebase settings
-        2. Project settings
-        3. Public settings
-        4. Edit Public facing-name to the app name
+ 1. firebase settings
+ 2. Project settings
+ 3. Public settings
+ 4. Edit Public facing-name to the app name
  */
 
 struct ForgotPasswordView: View {
     @State var user: UserViewModel = UserViewModel()
     @Environment(\.presentationMode) var presentationMode
     
-    
-   // @EnvironmentObject var userInfo: UserInfo
-   
-    
-    
-    
+    @State private var showAlert = false
+    @State private var errorString : String?
     
     var body: some View {
         NavigationView {
@@ -33,6 +29,16 @@ struct ForgotPasswordView: View {
                 TextField("Enter email address", text: $user.email).autocapitalization(.none).keyboardType(.emailAddress)
                 Button(action: {
                     // Reset Password action
+                    FBAuth.resetPassword(email: self.user.email) { (result) in
+                        switch result {
+                        case .failure(let error) :
+                            self.errorString = error.localizedDescription
+                        case .success(_) :
+                            break
+                        }
+                        self.showAlert = true
+                    }
+                    
                 }) {
                     Text("Reset")
                         .frame(width: 200)
@@ -45,12 +51,18 @@ struct ForgotPasswordView: View {
                 .disabled(!user.isEmailValid(_email: user.email))
                 Spacer()
             }.padding(.top)
-                .frame(width: 300)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+            .frame(width: 300)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
             .navigationBarTitle("Request a password reset", displayMode: .inline)
-                .navigationBarItems(trailing: Button("Dismiss") {
+            .navigationBarItems(trailing: Button("Dismiss") {
+                self.presentationMode.wrappedValue.dismiss()
+            })
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Password Reset"), message: Text(self.errorString ?? "Success. Reset email sent successfully.  Check your Email"), dismissButton: .default(Text("OK")) {
                     self.presentationMode.wrappedValue.dismiss()
+                    
                 })
+            }
         }
     }
 }
